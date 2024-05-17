fbb96124d6065fc698b5c998a642eca4ee4a5b00
leminhhehe <buileminh2405@gmail.com>
2024-05-17 12:28:11 +0700

    Change some texts

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const Product = require("./models/product.models.js");
 const productRoute = require("./routes/product.route");
 const app = express();
 
-//middleware
+// Load environment variables from .env file
+dotenv.config();
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//routes
-app.use('/api/product', productRoute);
+// Routes
+app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API Service");
 });
 
-
-
 // Kết nối tới MongoDB trước khi khởi động server
-mongoose
-  .connect(
-    "mongodb+srv://buileminh2405:LCVPeSBBHALCvW7n@backenddb.4d94imd.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB",
-    {}
-  )
+mongoose.connect(process.env.MONGODB_URL, {
+})
   .then(() => {
     console.log("Connected to Database");
+
     app.listen(3000, () => {
       console.log("Server is running on port 3000");
     });
@@ -32,9 +32,3 @@ mongoose
   .catch((error) => {
     console.log("Connection to Database Failed", error);
   });
-
-
-
-
-
-
